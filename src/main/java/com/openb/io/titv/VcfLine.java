b35fcc2700a3508433c8331c56dc32184e7d6bde
arrogantrobot <arrogantrobot@gmail.com>
2013-06-26 20:58:43 -0500

    First commit

---
+++ src/main/java/com/openb/io/titv/VcfLine.java
@@ -0,0 +1,66 @@
+package com.openb.io.titv;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class VcfLine {
+    private String ref;
+    private List<String> alts;
+    private BigInteger pos;
+    private String chrom;
+
+    public VcfLine(String line) {
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        List<String> vcfFields = new ArrayList<String>();
+        while (tokenizer.hasMoreTokens()) {
+            vcfFields.add(tokenizer.nextToken());
+        }
+        setChrom(vcfFields.get(0));
+        setPos(new BigInteger(vcfFields.get(1)));
+        setRef(vcfFields.get(3));
+        alts = new ArrayList<String>(Arrays.asList(vcfFields.get(4).split(",")));
+    }
+
+    public List<String> getTiTv() {
+        List<String> answer = new ArrayList<String>();
+        for (String alt : getAlts()) {
+            answer.add(getRef()+" -> "+alt);
+        }
+        return answer;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public List<String> getAlts() {
+        return alts;
+    }
+
+    public void setAlts(List<String> alts) {
+        this.alts = alts;
+    }
+
+    public BigInteger getPos() {
+        return pos;
+    }
+
+    public void setPos(BigInteger pos) {
+        this.pos = pos;
+    }
+
+    public String getChrom() {
+        return chrom;
+    }
+
+    public void setChrom(String chrom) {
+        this.chrom = chrom;
+    }
+}
